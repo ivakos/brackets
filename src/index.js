6571e64f761cca1e107b37def44be1cab97963e3
Ivan Kasenka <kasenka.ivan@gmail.com>
2023-02-27 05:16:42 +0300

    feat: final app

--- src/index.js
+++ src/index.js
@@ -9,35 +9,24 @@ module.exports = function check(str, bracketsConfig) {
     closeBrackets.push(bracketsConfig[i][1]);
   }
 
-  let counter = 0;
-
   for (let i = 0; i < str.length; i++) {
     let currentSymbol = str[i];
 
-    if (openBrackets.includes(currentSymbol) && closeBrackets.includes(currentSymbol)) {
-      counter++;
-    }
 
-    if ((openBrackets.includes(currentSymbol) && !closeBrackets.includes(currentSymbol)) || (openBrackets.includes(currentSymbol) && counter == 1)) {
+    if (stack.length === 0) {
       stack.push(currentSymbol);
     } else {
 
       let topElement = stack[stack.length - 1];
-      let indexOpenBrackets = openBrackets.indexOf(topElement);
-      let indexCloseBrackets = closeBrackets.indexOf(currentSymbol);
 
-      if (openBrackets.includes(currentSymbol) && closeBrackets.includes(currentSymbol)) {
-        counter = 0;
-      }
-
-      if (indexOpenBrackets === indexCloseBrackets) {
+      if (topElement === openBrackets[closeBrackets.indexOf(currentSymbol)] ) {
         stack.pop();
       }
       else {
-        return false;
+        stack.push(currentSymbol);
       }
     }
   }
-
+console.log(stack)
   return stack.length === 0;
 }
