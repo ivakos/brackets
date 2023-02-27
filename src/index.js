6b67a6e3ab5e4e423268112779d64b1a5a7f458c
Ivan Kasenka <kasenka.ivan@gmail.com>
2023-02-27 04:43:28 +0300

    feat: add task without 2 test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  let openBrackets = [];
+  let closeBrackets = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
+  }
+
+  let counter = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (openBrackets.includes(currentSymbol) && closeBrackets.includes(currentSymbol)) {
+      counter++;
+    }
+
+    if ((openBrackets.includes(currentSymbol) && !closeBrackets.includes(currentSymbol)) || (openBrackets.includes(currentSymbol) && counter == 1)) {
+      stack.push(currentSymbol);
+    } else {
+
+      let topElement = stack[stack.length - 1];
+      let indexOpenBrackets = openBrackets.indexOf(topElement);
+      let indexCloseBrackets = closeBrackets.indexOf(currentSymbol);
+
+      if (openBrackets.includes(currentSymbol) && closeBrackets.includes(currentSymbol)) {
+        counter = 0;
+      }
+
+      if (indexOpenBrackets === indexCloseBrackets) {
+        stack.pop();
+      }
+      else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
